646ef54a97ceca2d60d5b8b2c0e3c4e5742c4ed2
Valerio Salzano <vlr.slzn@gmail.com>
2023-05-04 15:39:43 +0200

    bonus 1: genres as array

--- index.php
+++ index.php
@@ -3,16 +3,16 @@ class Movie {
 
   public $title;
   public $language;
-  public $genre;
+  public $genres;
   public $release_year;
   public $vote_avg;
   public $poster;
   public $plot;
 
-  public function __construct($title, $language, $genre, $release_year, $poster, $vote_avg = null, $plot = null) {
+  public function __construct($title, $language,array $genres, $release_year, $poster, $vote_avg = null, $plot = null) {
     $this->title = $title;
     $this->language = $language;
-    $this->genre = $genre;
+    $this->genres = $genres;
     $this->release_year = $release_year;
     $this->poster = $poster;
     $this->vote_avg = $vote_avg;
@@ -23,7 +23,7 @@ class Movie {
     $overview = "
       Titolo: $this->title
       Lingua: $this->language
-      Genere: $this->genre
+      Generi: ".join(', ',$this->genres)."
       Anno: $this->release_year
       Trama: $this->plot
       ";
@@ -34,9 +34,11 @@ class Movie {
 
 $movie_1_plot = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas blanditiis minima, sint ut accusamus reprehenderit debitis error quasi laborum vero nobis in eum quam eveniet aspernatur repudiandae asperiores recusandae neque.";
 $movie_2_plot = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, sit a atque excepturi ipsam quam dolore minima aperiam totam asperiores consequatur ad fuga doloremque illo rem repellat libero nemo laborum.";
+$movie_1_genres = ['fantasy', 'avventura', 'commedia'];
+$movie_2_genres = ['azione','thriller','fantascienza'];
 
-$movie_1 = new Movie("Dungeons & Dragons - L'onore dei ladri","IT","fantasy","2023","movie_1_poster_link", 3.5 ,$movie_1_plot);
-$movie_2 = new Movie("V per Vendetta","IT","azione","2006","movie_2_poster_link", 4.5, $movie_2_plot);
+$movie_1 = new Movie("Dungeons & Dragons - L'onore dei ladri","IT",$movie_1_genres,"2023","movie_1_poster_link", 3.5 ,$movie_1_plot);
+$movie_2 = new Movie("V per Vendetta","IT",$movie_2_genres,"2006","movie_2_poster_link", 4.5, $movie_2_plot);
 
 ?>
 
